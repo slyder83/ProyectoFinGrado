d15532fc83379f291090c2513da911d82463ea09
slyder83 <rcenegar@gmail.com>
2025-05-22 11:52:26 +0200

    Pagina de login hecha
    Pagina de login con los estilos terminada

--- login.php
+++ login.php
@@ -7,18 +7,126 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina de login</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h2>Acceso al panel del restaurante</h2>
-    <!-- Formulario de login -->
-    <form action="verificar_login.php" method="POST">
-        <label for="email">Correo electrónico:</label>
-        <input type="email" name="email" id="email" required>
+    <header>
+        <img src="assets/imagenes/minha-baek-Z073P3hgGdM-unsplash.jpg" alt="Foto de Minha Baek en Unsplash" width="2400" height="1501">
+        <div class="header__opacidad"></div>
+        <h1 class="header__titulo">Login de personal</h1>
+    </header>
+    
+    <nav id="navbar">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="formulario_reserva.php">Reservas</a></li>
+            <li><a href="#sobre-nosotros">Sobre nosotros</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
 
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password" required>
+    <div id="nav-placeholder"></div>
 
-        <input type="submit" value="Iniciar sesión">
+    <main>
+        <div class="pagina-formulario">
+            <div class="formulario__contenido ancho-max sombra">
+                <div class="formulario__cabecera">
+                    <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                    <svg  class="icono" ="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm161.5-86.1c-12.2-5.2-26.3 .4-31.5 12.6s.4 26.3 12.6 31.5l11.9 5.1c17.3 7.4 35.2 12.9 53.6 16.3l0 50.1c0 4.3-.7 8.6-2.1 12.6l-28.7 86.1c-4.2 12.6 2.6 26.2 15.2 30.4s26.2-2.6 30.4-15.2l24.4-73.2c1.3-3.8 4.8-6.4 8.8-6.4s7.6 2.6 8.8 6.4l24.4 73.2c4.2 12.6 17.8 19.4 30.4 15.2s19.4-17.8 15.2-30.4l-28.7-86.1c-1.4-4.1-2.1-8.3-2.1-12.6l0-50.1c18.4-3.5 36.3-8.9 53.6-16.3l11.9-5.1c12.2-5.2 17.8-19.3 12.6-31.5s-19.3-17.8-31.5-12.6L338.7 175c-26.1 11.2-54.2 17-82.7 17s-56.5-5.8-82.7-17l-11.9-5.1zM256 160a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/></svg>
+                    <h2 class="titulo">Acceso al panel de control</h2>
+                    <p class="descripcion">Introduce las credenciales de acceso</p>
+                </div>
+                <!-- Formulario para el login -->
+                <form action="verificar_login.php" method="POST" class="formulario__grupos">
+                    <div class="formulario__grupo">
+                        <label for="email" class="label__icono">
+                            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                            <svg class="icono" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                            Correo electrónico:
+                        </label>
+                        <input type="email" name="email" id="email" required>
+                    </div>
+
+                    <div class="formulario__grupo">
+                        <label for="password" class="label__icono">
+                            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                            <svg class="icono" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M336 352c97.2 0 176-78.8 176-176S433.2 0 336 0S160 78.8 160 176c0 18.7 2.9 36.8 8.3 53.7L7 391c-4.5 4.5-7 10.6-7 17l0 80c0 13.3 10.7 24 24 24l80 0c13.3 0 24-10.7 24-24l0-40 40 0c13.3 0 24-10.7 24-24l0-40 40 0c6.4 0 12.5-2.5 17-7l33.3-33.3c16.9 5.4 35 8.3 53.7 8.3zM376 96a40 40 0 1 1 0 80 40 40 0 1 1 0-80z"/></svg>
+                            Contraseña:
+                        </label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
+            
+            
+                    <button type="submit" class="boton-formulario">
+                        <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                        <svg class="icono" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg>
+                        Iniciar Sesión
+                    </button>
+            </div>
+        </div>
+
+
+    </main>
+
+    <footer>
+        <div class="footer__contenido ancho-max">
+            <div class="footer__informacion">
+                <div class="footer__tarjeta">
+                    <h4>Horario</h4>
+                    <p class="small">Martes a Viernes: 12:00 - 23:00</p>
+                    <p class="small">Sabados y Domingos: 13:00 - 23:00</p>
+                    <p class="small">Lunes: Cerrado</p>
+                </div>
+
+                <div class="footer__tarjeta">
+                    <h4>Dirección</h4>
+                    <p class="small">Calle de la Gastronomía, 123</p>
+                    <p class="small">Madrid, España</p>
+                    <p class="small"><a href="https://www.google.com/maps?q=Calle+de+la+Gastronomía,+123,+Madrid,+España" target="_blank" rel="noopener noreferrer">Ver en Google Maps</a></p>
+                </div>
+
+                <div class="footer__tarjeta">
+                    <h4>Contacto</h4>
+                    <p class="small">+34 123 456 789</p>
+                    <p class="small"><a href="mailto:info@restauranteasia.com">info@restauranteasia.com</a></p>
+                </div>
+
+                <div class="footer__tarjeta">
+                    <h4>Redes Sociales</h4>
+                    <div class="footer__redes-sociales">
+                        <a href="#" title="Facebook" aria-label="Facebook">
+                            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                            <svg class="red-social" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
+                        </a>
+                        <a href="#" title="Instagram" aria-label="Instagram">
+                            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                            <svg class="red-social" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                        </a>
+                        <a href="#" title="X" aria-label="X">
+                            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                            <svg class="red-social" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer__separador"></div>
+
+            <div class="footer__legal">
+                <p class="small">&copy; <span id="year"></span> Restaurante Asia Oriental. Todos los derechos reservados</p>
+                <div class="footer__politicas">
+                    <p class="small"><a href="politica_privacidad.html">Política de privacidad</a></p>
+                    <p class="small"><a href="terminos_condiciones.html">Términos y condiciones</a></p>
+                    <p class="small"><a href="cookies.html">Política de cookies</a></p>
+                    <p class="small"><a href="aviso_legal.html">Aviso legal</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- <script src="js/script.js"></script> -->
+    <script src="js/footer.js"></script>
+
 </body>
 </html>
